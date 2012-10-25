a890e114d82779b0c5ee82ba663ca218008c0596
Ariejan de Vroom <ariejan@ariejan.net>
2012-10-25 11:04:39 +0200

    Loaded form defintions in app/forms properly

--- lib/formulan.rb
+++ lib/formulan.rb
@@ -5,4 +5,19 @@ require 'active_model'
 require "formulan/engine"
 
 module Formulan
+  # Hash containing defined forms
+  def self.forms
+    @forms ||= {}
+  end
+
+  def self.fetch(identifier)
+    identifier = identifier.to_sym if identifier.is_a?(String)
+    forms.fetch(identifier, nil)
+  end
+
+  # Convenience method for Formulan::FormDefinition.new
+  def self.define(identifier, &block)
+    forms[identifier] = FormDefinition.new(identifier, &block)
+  end
+
 end
--- lib/formulan/engine.rb
+++ lib/formulan/engine.rb
@@ -5,5 +5,10 @@ module Formulan
     initializer :assets do |config|
       Rails.application.config.assets.precompile += %w(formulan.css formulan.js)
     end
+
+    config.after_initialize do
+      Dir[Rails.root.join("app/forms/**/*_form.rb")].each {|f| require f}
+    end
   end
 end
+
